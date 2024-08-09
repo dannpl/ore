c15f36f37694cca885d37d5fcc1e30ee3697d540
dannpl <dancardosobr@gmail.com>
2024-08-09 00:59:47 -0300

    improve bus

--- src/mine.rs
+++ src/mine.rs
@@ -3,8 +3,9 @@ use drillx::{Hash, Solution};
 use futures::StreamExt;
 use ore_api::{
     consts::{BUS_ADDRESSES, BUS_COUNT},
-    state::Proof,
+    state::{Bus, Proof},
 };
+use ore_utils::AccountDeserialize;
 use rand::Rng;
 use rayon::prelude::*;
 use solana_program::pubkey::Pubkey;
@@ -95,7 +96,7 @@ impl Miner {
             ixs.push(ore_api::instruction::mine(
                 signer.pubkey(),
                 signer.pubkey(),
-                find_bus(),
+                self.find_bus().await,
                 solution,
             ));
 
@@ -226,12 +227,29 @@ impl Miner {
             .saturating_sub(clock.unix_timestamp)
             .max(0) as u64
     }
-}
 
-// TODO Pick a better strategy (avoid draining bus)
-fn find_bus() -> Pubkey {
-    let i = rand::thread_rng().gen_range(0..BUS_COUNT);
-    BUS_ADDRESSES[i]
+    async fn find_bus(&self) -> Pubkey {
+        // Fetch the bus with the largest balance
+        if let Ok(accounts) = self.rpc_client.get_multiple_accounts(&BUS_ADDRESSES).await {
+            let mut top_bus_balance: u64 = 0;
+            let mut top_bus = BUS_ADDRESSES[0];
+            for account in accounts {
+                if let Some(account) = account {
+                    if let Ok(bus) = Bus::try_from_bytes(&account.data) {
+                        if bus.rewards.gt(&top_bus_balance) {
+                            top_bus_balance = bus.rewards;
+                            top_bus = BUS_ADDRESSES[bus.id as usize];
+                        }
+                    }
+                }
+            }
+            return top_bus;
+        }
+
+        // Otherwise return a random bus
+        let i = rand::thread_rng().gen_range(0..BUS_COUNT);
+        BUS_ADDRESSES[i]
+    }
 }
 
 fn calculate_multiplier(balance: u64, top_balance: u64) -> f64 {
